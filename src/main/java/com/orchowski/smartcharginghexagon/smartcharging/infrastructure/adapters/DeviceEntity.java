144227dbdd0caadfdb46ab4ee1f344070612af51
Tomasz Orchowski <tomasz.orchowski@codibly.com>
2022-12-29 16:05:31 +0100

    tests improvement

--- src/main/java/com/orchowski/smartcharginghexagon/smartcharging/infrastructure/adapters/DeviceEntity.java
+++ src/main/java/com/orchowski/smartcharginghexagon/smartcharging/infrastructure/adapters/DeviceEntity.java
@@ -4,13 +4,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Document("Device")
 public class DeviceEntity {
     @Id
     private String id;
